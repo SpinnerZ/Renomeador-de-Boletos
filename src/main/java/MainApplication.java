ae5f5bce281a76b133ee38f01fe93bfb49f489d8
ladsouza <leonardo.a.a.souza@accenture.com>
2021-05-22 18:55:21 -0300

    Correção bug pula primeira página

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,88 +1,88 @@
-import boleto.BoletoHandler;
-import org.apache.pdfbox.pdmodel.PDDocument;
-
-import java.io.IOException;
-import java.util.Calendar;
-import java.util.Iterator;
-
 import static util.PDFHandler.loadFile;
 import static util.PDFHandler.splitPDF;
 
+import boleto.BoletoHandler;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
 public class MainApplication {
 
-    static PDDocument boletoDocument;
-    static PDDocument nfeDocument;
-    static String saveDirectory;
-    static boolean createUserFolder;
-    static boolean containsNFe;
+  static PDDocument boletoDocument;
+  static PDDocument nfeDocument;
+  static String saveDirectory;
+  static boolean createUserFolder;
+  static boolean containsNFe;
 
-    public static void main(String[] args) {
-        setupPaths(args);
+  public static void main(String[] args) {
+    setupPaths(args);
 
-        try {
-            doBoletoOperations();
-        } catch (IOException e) {
-            System.out.println("Ocorreu algum erro na operação com boletos: " + e.getMessage());
-            e.printStackTrace();
-        }
+    try {
+      doBoletoOperations();
+    } catch (IOException e) {
+      System.out.println("Ocorreu algum erro na operação com boletos: " + e.getMessage());
+      e.printStackTrace();
+    }
 
 //        try {
 //            doNFeOperations();
 //        }
-    }
+  }
 
 //    static void doNFeOperations() {
 //
 //    }
 
-    static void doBoletoOperations() throws IOException {
-        Iterator<PDDocument> iterator = splitPDF(boletoDocument).listIterator();
+  static void doBoletoOperations() throws IOException {
+    List<PDDocument> documentPages = splitPDF(boletoDocument);
 
-        if (iterator.hasNext()) {
-            BoletoHandler.boletoHandler(saveDirectory, createUserFolder, iterator);
-        } else {
-            System.out.println("O arquivo .pdf não contém páginas");
-            System.exit(0);
-        }
-        boletoDocument.close();
+    if (documentPages.isEmpty()) {
+      System.out.println("O arquivo .pdf não contém páginas");
+      System.exit(0);
+    } else {
+      BoletoHandler.boletoHandler(saveDirectory, createUserFolder, documentPages);
+    }
+    boletoDocument.close();
+  }
+
+  static void setupPaths(String[] args) {
+
+    if (args.length < 1) {
+      System.out.println(
+          "Precisa inserir por argumentos ao menos a pasta de origem do arquivo que contém todos os boletos");
+      System.exit(0);
     }
 
-    static void setupPaths(String[] args) {
+    String boletoOriginDirectory = args[0] + "\\";
+    boletoDocument = loadFile(boletoOriginDirectory);
 
-        if (args.length < 1) {
-            System.out.println("Precisa inserir por argumentos ao menos a pasta de origem do arquivo que contém todos os boletos");
-            System.exit(0);
-        }
+    switch (args.length) {
+      case 1:
+        saveDirectory = boletoOriginDirectory;
+        createUserFolder = false;
+        containsNFe = false;
+        break;
 
-        String boletoOriginDirectory = args[0] + "\\";
-        boletoDocument = loadFile(boletoOriginDirectory);
+      case 2:
+        saveDirectory = args[1] + "\\" + (Calendar.getInstance().get(Calendar.MONTH) + 1) + "\\";
+        createUserFolder = true;
+        containsNFe = false;
+        break;
 
-        switch (args.length) {
-            case 1:
-                saveDirectory = boletoOriginDirectory;
-                createUserFolder = false;
-                containsNFe = false;
-                break;
+      case 3:
+        saveDirectory = args[1] + "\\" + (Calendar.getInstance().get(Calendar.MONTH) + 1) + "\\";
+        createUserFolder = true;
+        containsNFe = true;
+        String nfeOriginDirectory = args[2] + "\\";
+        nfeDocument = loadFile(nfeOriginDirectory);
+        break;
 
-            case 2:
-                saveDirectory = args[1] + "\\" + (Calendar.getInstance().get(Calendar.MONTH) + 1) +"\\";
-                createUserFolder = true;
-                containsNFe = false;
-                break;
-
-            case 3:
-                saveDirectory = args[1] + "\\" + (Calendar.getInstance().get(Calendar.MONTH) + 1) +"\\";
-                createUserFolder = true;
-                containsNFe = true;
-                String nfeOriginDirectory = args[2] + "\\";
-                nfeDocument = loadFile(nfeOriginDirectory);
-                break;
-
-            default:
-                System.out.println("O programa aceita 1, 2 ou 3 argumentos, sendo eles em ordem:\n" +
-                        "Pasta de origem do arquivo que contém todos os boletos\n" +
-                        "Pasta de destino se quiser salvar automaticamente o mês e uma pasta por cliente\n" +
-                        "Pasta de origem do arquivo que contém o talão de NFe");
-        }
+      default:
+        System.out.println("O programa aceita 1, 2 ou 3 argumentos, sendo eles em ordem:\n" +
+            "Pasta de origem do arquivo que contém todos os boletos\n" +
+            "Pasta de destino se quiser salvar automaticamente o mês e uma pasta por cliente\n" +
+            "Pasta de origem do arquivo que contém o talão de NFe");
     }
+  }
 }
--- src/main/java/boleto/BoletoHandler.java
+++ src/main/java/boleto/BoletoHandler.java
@@ -1,63 +1,56 @@
 package boleto;
 
 import boleto.banks.BankInterface;
-import org.apache.pdfbox.pdmodel.PDDocument;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class BoletoHandler {
 
-    private BoletoHandler() {}
+  private BoletoHandler() {
+  }
 
-    public static void boletoHandler(
-            String saveDirectory, boolean userFolder, Iterator<PDDocument> iterator) throws IOException {
+  public static void boletoHandler(String saveDirectory, boolean userFolder, List<PDDocument> pages)
+      throws IOException {
+    final BankInterface BANK = BankInterface.getBank(pages.get(0));
+    List<String> payers = new ArrayList<>();
+    int count;
 
-        PDDocument document = iterator.next();
-        final BankInterface  BANK     = BankInterface.getBank(document);
-        List<String>         payers   = new ArrayList<>();
-        int                  count;
+    for (PDDocument page : pages) {
+      String payer = BANK.getPayer(page).replace("/", "");
+      payers.add(payer);
+      count = (int) payers.stream().filter(p -> p.equals(payer)).count();
 
-        while (true) {
-            String     payer    = BANK.getPayer(document).replace("/", "");
-            payers.add(payer);
-            count = (int) payers.stream().filter(p -> p.equals(payer)).count();
+      saveBoleto(page, saveDirectory, userFolder, payer, count);
 
-            saveBoleto(document, saveDirectory, userFolder, payer, count);
-            document.close();
+      page.close();
+    }
+  }
 
-            if (iterator.hasNext()) {
-                document = iterator.next();
-            } else {
-                break;
-            }
-        }
+  static void saveBoleto(PDDocument document, String saveDirectory, boolean userFolder,
+      String payer, int count)
+      throws IOException {
+    if (userFolder) {
+      saveDirectory += payer + "\\";
+
+      //Adds the user name to the save path
+      try {
+        Path path = Paths.get(saveDirectory);
+        Files.createDirectories(path);
+      } catch (IOException e) {
+        System.out.println("A pasta não pôde ser criada!\n" + e.getMessage());
+        System.exit(0);
+      }
     }
 
-    static void saveBoleto(PDDocument document, String saveDirectory, boolean userFolder, String payer, int count)
-            throws IOException {
-        if (userFolder) {
-            saveDirectory += payer + "\\";
-
-            //Adds the user name to the save path
-            try {
-                Path path = Paths.get(saveDirectory);
-                Files.createDirectories(path);
-            } catch (IOException e) {
-                System.out.println("A pasta não pôde ser criada!\n" + e.getMessage());
-                System.exit(0);
-            }
-        }
-
-        if (count > 1) {
-            document.save(saveDirectory + payer + " " + count + ".pdf");
-        } else {
-            document.save(saveDirectory + payer + ".pdf");
-        }
+    if (count > 1) {
+      document.save(saveDirectory + payer + " " + count + ".pdf");
+    } else {
+      document.save(saveDirectory + payer + ".pdf");
     }
+  }
 }
